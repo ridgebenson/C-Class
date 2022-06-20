c284b01967dc5a01aae06d2abc6844e8d1bc0c54
RBeNturi <ridgemuturi@gmail.com>
2022-06-20 19:26:18 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int x=100;
+int y=1;
+int result;
+//Console.WriteLine("The result is {0}",result);
+try{
+    result=x/y;
+Console.WriteLine("The result is {0}",result);
+}
+catch{
+    Console.WriteLine("Error!");
+}
